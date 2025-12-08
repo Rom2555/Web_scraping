03ec722f0e11649978436da805df8b3e8c704561
Roman E <rk2555@yandex.ru>
2025-12-08 19:07:54 +0300

    no_commit

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ import json
 
 response = requests.get('https://habr.com/ru/articles/')
 soup = bs4.BeautifulSoup(response.text, features='lxml')
+#print(soup)
 
-articles_block = soup.select_one('div.tm-articles-list')
+articles = soup.select('article.tm-articles-list__item')
+print(articles)
+
+# parsed_data = []
+# for article in articles_list:
+#     if article.select_one('span.post__title-label'):
+#         title = article.select_one('span.post__title-text').text.strip()
+#         link = article.select_one('a.post__title_link')['href']
+#         time = article.select_one('span.post__time').text.strip()
+#         text = article.select_one('div.post__text').text.strip()
+#
+#         parsed_data.append({title, link, time, text,})
+#
+# print(parsed_data)
+
+#
+
+#articles_block = soup.select('div.tm-articles-list')
 
 
-articles_list = articles_block.select('tm-articles-list__item')
-print(articles_list)
+#articles_list = articles_block.select_one('tm-articles-list__item')
+#print(articles_block)
 #
 # parsed_data = []
 # for article in articles_list:
